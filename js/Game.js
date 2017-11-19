5be83f28e29caa9ab6f763f17abc59f145b4f22c
DavidNovo <david.novogrodsky@gmail.com>
2017-11-19 16:12:32 -0600

    now world wraps and is infinte

--- js/Game.js
+++ js/Game.js
@@ -1,7 +1,6 @@
 var BasicGame = BasicGame || {};
 
-console.log("Game!!!!!!!!!");
-BasicGame.Game = function() {
+BasicGame.Game = function () {
 
     //  When a State is added to Phaser it automatically has the following 
     // properties set on it, even if they already exist:
@@ -30,76 +29,111 @@ BasicGame.Game = function() {
 
 BasicGame.Game.prototype = {
 
-    preload: function() {
+    preload: function () {
         this.game.time.advancedTiming = true;
     },
 
-    create: function() {
+    create: function () {
 
-    //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+        // sounds
+        this.barkSound = this.game.add.audio('bark');
+        this.whineSound = this.game.add.audio('whine');
 
-    //set up background and ground layer
-    this.game.world.setBounds(0, 0, 3500, this.game.height);
-    this.grass = this.add.tileSprite(0,this.game.height-100,this.game.world.width,70,'grass');
-    this.ground = this.add.tileSprite(0,this.game.height-70,this.game.world.width,70,'ground');
-    
-    //create player and walk animation
-    this.player = this.game.add.sprite(this.game.width/2, this.game.height-90, 'dog');
-    this.player.animations.add('walk');
-    
-    //create the fleas
-    this.generateFleas();
-    //and the toy mounds
-    this.generateMounds();
-    
-    //put everything in the correct order (the grass will be camoflauge),
-    //but the toy mounds have to be above that to be seen, but behind the
-    //ground so they barely stick up
-    this.game.world.bringToTop(this.grass);
-    this.game.world.bringToTop(this.mounds);
-    this.game.world.bringToTop(this.ground);
-    //this.game.world.bringToTop(this.player);
- 
-    
-    //enable physics on the player and ground
-    this.game.physics.arcade.enable(this.player);
-    this.game.physics.arcade.enable(this.ground);
- 
-    //player gravity
-    this.player.body.gravity.y = 1000;
-    
-    //so player can walk on ground
-    this.ground.body.immovable = true;
-    this.ground.body.allowGravity = false;
+        // variables used for the game state
+        this.scratches = 0;
+        this.wraps = 0;
+        this.points = 0;
+        this.wrapping = true;
+        this.stopped = false;
+        this.maxScratches = 5;
 
-    //properties when the player is digging, scratching and standing, so we can use in update()
-    var playerDigImg = this.game.cache.getImage('playerDig');
-    this.player.animations.add('dig');
-    this.player.digDimensions = {width: playerDigImg.width, height: playerDigImg.height};
-    
-    var playerScratchImg = this.game.cache.getImage('playerScratch');
-    this.player.animations.add('scratch');
-    this.player.scratchDimensions = {width: playerScratchImg.width, height: playerScratchImg.height};
-    
-    this.player.standDimensions = {width: this.player.width, height: this.player.height};
-    this.player.anchor.setTo(0.5, 1);
-    
-    //the camera will follow the player in the world
-    this.game.camera.follow(this.player);
-    
-    //play the walking animation
-    this.player.animations.play('walk', 3, true);
- 
-    //move player with cursor keys
-    this.cursors = this.game.input.keyboard.createCursorKeys();
-    
-    //...or by swiping
-    this.swipe = this.game.input.activePointer;
-    },
+        // create array for toys that can be gathered from the mounds
+        var bone = this.game.add.sprite(0, this.game.height - 130, 'bone');
+        var ball = this.game.add.sprite(0, this.game.height - 130, 'ball');
+        bone.visible = false;
+        ball.visible = false;
+        this.toys = [bone, ball];
+        this.currentToy = bone;
 
-    update: function() {
+        // display the stats of the player
+        var style1 = { font: "20px Arial", fill: "#ff0" };
+        var t1 = this.game.add.text(10, 20, "Points:", style1);
+        var t2 = this.game.add.text(this.game.width - 300, 20, "Remaining Flea Scratches:", style1);
+        t1.fixedToCamera = true;
+        t2.fixedToCamera = true;
+
+        var style2 = { font: "26px Arial", fill: "#00ff00" };
+        this.pointsText = this.game.add.text(80, 18, "", style2);
+        this.fleasText = this.game.add.text(this.game.width - 50, 18, "", style2);
+        this.refreshStats();
+        this.pointsText.fixedToCamera = true;
+        this.fleasText.fixedToCamera = true;
+
 
         //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+
+        //set up background and ground layer
+        this.game.world.setBounds(0, 0, 3500, this.game.height);
+        this.grass = this.add.tileSprite(0, this.game.height - 100, this.game.world.width, 70, 'grass');
+        this.ground = this.add.tileSprite(0, this.game.height - 70, this.game.world.width, 70, 'ground');
+
+        //create player and walk animation
+        this.player = this.game.add.sprite(this.game.width / 2, this.game.height - 90, 'dog');
+        this.player.animations.add('walk');
+
+        //create the fleas
+        this.generateFleas();
+        //and the toy mounds
+        this.generateMounds();
+
+        //put everything in the correct order (the grass will be camoflauge),
+        //but the toy mounds have to be above that to be seen, but behind the
+        //ground so they barely stick up
+        this.game.world.bringToTop(this.grass);
+        this.game.world.bringToTop(this.mounds);
+        this.game.world.bringToTop(this.ground);
+
+
+        //enable physics on the player and ground
+        this.game.physics.arcade.enable(this.player);
+        this.game.physics.arcade.enable(this.ground);
+
+        //player gravity
+        this.player.body.gravity.y = 1000;
+
+        //so player can walk on ground
+        this.ground.body.immovable = true;
+        this.ground.body.allowGravity = false;
+
+        //properties when the player is digging, scratching and standing, so we can use in update()
+        var playerDigImg = this.game.cache.getImage('playerDig');
+        this.player.animations.add('dig');
+        this.player.digDimensions = { width: playerDigImg.width, height: playerDigImg.height };
+
+        var playerScratchImg = this.game.cache.getImage('playerScratch');
+        this.player.animations.add('scratch');
+        this.player.scratchDimensions = { width: playerScratchImg.width, height: playerScratchImg.height };
+
+        this.player.standDimensions = { width: this.player.width, height: this.player.height };
+        this.player.anchor.setTo(0.5, 1);
+
+        //the camera will follow the player in the world
+        this.game.camera.follow(this.player);
+
+        //play the walking animation
+        this.player.animations.play('walk', 3, true);
+
+        //move player with cursor keys
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+
+        //...or by swiping
+        this.swipe = this.game.input.activePointer;
+    },
+
+    update: function () {
+
+        //  Honestly, just about anything could go here. It's YOUR game after all. 
+        // Eat your heart out!
 
         // player land on ground instead of falling through
         this.game.physics.arcade.collide(this.player, this.ground, this.playerHit, null, this);
@@ -111,11 +145,32 @@ BasicGame.Game.prototype = {
         this.game.physics.arcade.overlap(this.player, this.mounds, this.collect, this.checkDig, this);
 
         //only respond to keys and keep the speed if the player is alive
-        if(this.player.alive && !this.stopped) {
-            
-            this.player.body.velocity.x = 200;
+        if (this.player.alive && !this.stopped) {
 
-             //take the appropriate action for swiping up or pressing up arrow on keyboard
+            this.player.body.velocity.x = 200;
+            //We do a little math to determine whether the game world has wrapped around.
+            //If so, we want to destroy everything and regenerate, so the game will remain random
+            if (!this.wrapping && this.player.x < this.game.width) {
+                //Not used yet, but may be useful to know how many times we've wrapped
+                this.wraps++;
+
+                // once we wrap we want to destroy everything and regenerate the world
+                this.wrapping = true;
+                this.fleas.destroy();
+                this.generateFleas();
+                this.mounds.destroy();
+                this.generateMounds();
+
+                // then put things back in the correct order
+                this.game.world.bringToTop(this.grass);
+                this.game.world.bringToTop(this.mounds);
+                this.game.world.bringToTop(this.ground);
+            } else if (this.player.x >= this.game.width) {
+                this.wrapping = false;
+            }
+
+
+            //take the appropriate action for swiping up or pressing up arrow on keyboard
             //we don't wait until the swipe is finished (this.swipe.isUp),
             //  because of latency problems (it takes too long to jump before hitting a flea)
             if (this.swipe.isDown && (this.swipe.positionDown.y > this.swipe.position.y)) {
@@ -124,65 +179,75 @@ BasicGame.Game.prototype = {
             else if (this.cursors.up.isDown) {
                 this.playerJump();
             }
+
+            //The game world is infinite in the x-direction, so we wrap around.
+            //We subtract padding so the player will remain in the middle of the screen when
+            //wrapping, rather than going to the end of the screen first.
+            this.game.world.wrap(this.player, -(this.game.width / 2), false, true, false);
+
         }
 
     },
 
-    render: function() {
+    render: function () {
         //this.game.debug.text(this.game.time.fps || '--', 20, 70, "#00ff00", "40px Courier");   
     },
 
-    generateMounds: function() {
+    generateMounds: function () {
         this.mounds = this.game.add.group();
-     
+
         //enable physics in them
         this.mounds.enableBody = true;
-     
+
         //phaser's random number generator
         var numMounds = this.game.rnd.integerInRange(0, 5)
         var mound;
-     
-        for (var i = 0; i < numMounds; i++) {
-          //add sprite within an area excluding the beginning and ending
-          //  of the game world so items won't suddenly appear or disappear when wrapping
-          var x = this.game.rnd.integerInRange(this.game.width, this.game.world.width - this.game.width);
-          mound = this.mounds.create(x, this.game.height-75, 'mound');
-          mound.body.velocity.x = 0;
-        }
-      },
 
-      generateFleas: function() {
+        for (var i = 0; i < numMounds; i++) {
+            //  add sprite within an area excluding the beginning and ending
+            //  of the game world so items won't suddenly appear or disappear when wrapping
+            var x = this.game.rnd.integerInRange(this.game.width, this.game.world.width - this.game.width);
+            mound = this.mounds.create(x, this.game.height - 75, 'mound');
+            mound.body.velocity.x = 0;
+        }
+    },
+
+    generateFleas: function () {
         this.fleas = this.game.add.group();
-        
+
         //enable physics in them
         this.fleas.enableBody = true;
-     
+
         //phaser's random number generator
         var numFleas = this.game.rnd.integerInRange(1, 5)
         var flea;
-     
+
         for (var i = 0; i < numFleas; i++) {
-          //add sprite within an area excluding the beginning and ending
-          //  of the game world so items won't suddenly appear or disappear when wrapping
-          var x = this.game.rnd.integerInRange(this.game.width, this.game.world.width - this.game.width);
-          flea = this.fleas.create(x, this.game.height-115, 'flea');
-     
-          //physics properties
-          flea.body.velocity.x = this.game.rnd.integerInRange(-20, 0);
-          
-          flea.body.immovable = true;
-          flea.body.collideWorldBounds = false;
+            //add sprite within an area excluding the beginning and ending
+            //  of the game world so items won't suddenly appear or disappear when wrapping
+            var x = this.game.rnd.integerInRange(this.game.width, this.game.world.width - this.game.width);
+            flea = this.fleas.create(x, this.game.height - 115, 'flea');
+
+            //physics properties
+            flea.body.velocity.x = this.game.rnd.integerInRange(-20, 0);
+
+            flea.body.immovable = true;
+            flea.body.collideWorldBounds = false;
         }
-      },
-
-      playerJump: function() {
+    },
+    playerJump: function () {
         //when the ground is a sprite, we need to test for "touching" instead of "blocked"
-        if(this.player.body.touching.down) {
-          this.player.body.velocity.y -= 700;
-        }    
-      },
+        if (this.player.body.touching.down) {
+            this.player.body.velocity.y -= 700;
+        }
+    },
 
-    quitGame: function(pointer) {
+    refreshStats: function () {
+        this.pointsText.text = this.points;
+        this.fleasText.text = this.maxScratches - this.scratches;
+    },
+
+    quitGame: function (pointer) {
 
         //  Here you should destroy anything you no longer need.
         //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
@@ -191,6 +256,6 @@ BasicGame.Game.prototype = {
         this.state.start('MainMenu');
 
     }
-        
+
 
 };
